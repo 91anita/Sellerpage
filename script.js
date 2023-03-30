522aec08b0d6fc13936cc11430d01a9bd915122f
91anita <45237162+91anita@users.noreply.github.com>
2023-03-30 19:01:55 +0530

    demomodified

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var total=0;
 async function submitdetails(event) {
     event.preventDefault()
 
@@ -11,7 +11,7 @@ async function submitdetails(event) {
     }
 
     try {
-        const response = await axios.post("https://crudcrud.com/api/a3f212160a354394b216a12c1dfa69df/admindata", obj)
+        const response = await axios.post("https://crudcrud.com/api/8c0dbd0385004bc9a8da7d653c179b90/admindata", obj)
         showuseronscreen(response.data)
         console.log(response)
     } catch (error) {
@@ -19,20 +19,35 @@ async function submitdetails(event) {
     }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    axios
-        .get("https://crudcrud.com/api/a3f212160a354394b216a12c1dfa69df/admindata")
-        .then((response) => {
+window.addEventListener("DOMContentLoaded", async() => {
+    async function getdetails(){
+    try{
+        const response =await axios.get("https://crudcrud.com/api/8c0dbd0385004bc9a8da7d653c179b90/admindata")
             for (let i = 0; i < response.data.length; i++) {
                 showuseronscreen(response.data[i])
             }
-        })
-        .catch((error) => {
+        }
+        catch(error){
             console.log(error)
-        })
+        }
+    }
+    getdetails()
 })
+async function deleteProduct(obj,parentelem,childelem)
+{
+    try{
+        const response=await axios.delete(`https://crudcrud.com/api/8c0dbd0385004bc9a8da7d653c179b90/admindata/${obj._id}`)
+        parentelem.removeChild(childelem)
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+}
 
 function showuseronscreen(obj) {
+    total+=parseInt(obj.sprice)
+   
     let parentelem = document.getElementById("user")
     let childelem = document.createElement("li")
     childelem.textContent = obj.sprice + " - " + obj.pname
@@ -41,20 +56,32 @@ function showuseronscreen(obj) {
     del.type = "button"
     del.value = "Delete Product"
 
-    // let totalvalue = document.getElementById("total")
-    // totalvalue.text=obj.sprice
+   
 
     del.onclick = function() {
-        axios
-            .delete(`https://crudcrud.com/api/a3f212160a354394b216a12c1dfa69df/${obj._id}`)
-            .then((response) => {
-                parentelem.removeChild(childelem)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+        deleteProduct(obj,parentelem,childelem)
     };
 
     childelem.appendChild(del)
     parentelem.appendChild(childelem)
+    showtotalprice()
+    
+    
+    
+}
+function showtotalprice()
+{
+    const parentelem=document.getElementById('total');
+    const childelem=`<h3>Total Price of Products: Rs ${total}
+    </h3>`
+
+parentelem.innerHTML = childelem;
+}
+function deleteproduct(){
+    total-=parseInt(obj.sprice)
+    const parentelem=document.getElementById('total');
+    const childelem=`<h3>Total Price of Products: Rs ${total}
+    </h3>`
+
+parentelem.innerHTML = childelem;
 }
